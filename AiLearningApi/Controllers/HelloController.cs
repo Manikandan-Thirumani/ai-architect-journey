025c4792ea80a5f476966efea00a8d6f602ca75d
LingamSuyambu <suyambulingam2025@gmail.com>
2026-05-24 11:35:04 +0530

    Initial AI Architect setup

---
+++ AiLearningApi/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AiLearningApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "AI Architect Journey Started";
+        }
+    }
+}
